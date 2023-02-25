e00069a1ad476809e926ca1106aae9c68a1acb68
Sanchit <sanchitgurav100@gmail.com>
2023-02-25 22:59:57 +0530

    changes

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -5,6 +5,8 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text("Hello"),
+    );
   }
 }
