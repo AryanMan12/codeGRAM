b2b24955818e4ed83c0b78358365a4aed179590a
meisdee <deepaligupta01703@gmail.com>
2023-03-01 20:25:06 +0530

    login image picker

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,10 +1,12 @@
 import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //sign up user
   Future<String> signUpUser({
@@ -12,19 +14,27 @@ class AuthMethods {
     required String password,
     required String username,
     required String bio,
-    required Uint8List file,
+    //required Uint8List file,
   }) async {
     String res = "Some Error Occured";
     try {
       if (email.isNotEmpty ||
           password.isNotEmpty ||
           username.isNotEmpty ||
-          bio.isNotEmpty ||
-          file != null) {
+          bio.isNotEmpty) {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
-
+        print(cred.user!.uid);
         //add user to our database
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+        });
+        res = "success";
       }
     } catch (err) {
       res = err.toString();
