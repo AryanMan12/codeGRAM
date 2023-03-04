7637acd8643067231c25b0dae7705c29b722ead3
Sanchit <sanchitgurav100@gmail.com>
2023-03-05 00:36:04 +0530

    created resposive ui

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codegram/utils/colors.dart';
+import 'package:codegram/utils/global_variables.dart';
 import 'package:codegram/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -9,19 +10,24 @@ class FeedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        centerTitle: false,
-        title: SvgPicture.asset(
-          'assets/codeGram.svg',
-          color: primaryColor,
-          height: 32,
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.messenger_outline))
-        ],
-      ),
+      appBar: width > webScreenSize
+          ? null
+          : AppBar(
+              backgroundColor: mobileBackgroundColor,
+              centerTitle: false,
+              title: SvgPicture.asset(
+                'assets/codeGram.svg',
+                color: primaryColor,
+                height: 32,
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.messenger_outline))
+              ],
+            ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
             .collection('posts')
@@ -35,9 +41,17 @@ class FeedScreen extends StatelessWidget {
             );
           }
           return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) =>
-                  PostCard(snap: snapshot.data!.docs[index].data()));
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: width > webScreenSize ? width * 0.3 : 0,
+                vertical: width > webScreenSize ? 15 : 0,
+              ),
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
         },
       ),
     );
