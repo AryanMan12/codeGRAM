eb11604eae68d57398ceb99ccccae2a5bfdb8913
AryanMan12 <aryanaryan1212.ag@gmail.com>
2023-03-17 15:10:58 +0530

    view projects almost done

--- lib/widgets/project_item.dart
+++ lib/widgets/project_item.dart
@@ -8,68 +8,82 @@ class ProjectItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 4.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: 80.0,
-            height: 80.0,
-            decoration: BoxDecoration(
-              color: secondaryColor,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: primaryColor,
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(snap['projPhotoUrl']),
+    return InkWell(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => DetailsScreen(
+            snap: snap,
+          ),
+        ),
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                color: secondaryColor,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(snap['projPhotoUrl']),
+                    ),
+                    borderRadius: BorderRadius.circular(16.0),
                   ),
-                  borderRadius: BorderRadius.circular(16.0),
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 12.0),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(width: 12.0),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    snap["projectName"],
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 18.0),
+                  ),
+                  Text(
+                    snap["description"],
+                    maxLines: 2,
+                    style: const TextStyle(
+                      fontSize: 12.0,
+                      color: secondaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
               children: [
                 Text(
-                  snap["projectName"],
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18.0),
+                  "${snap["likes"].length}",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
                 ),
                 Text(
-                  snap["description"],
-                  maxLines: 2,
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    color: secondaryColor,
-                  ),
+                  "likes",
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14),
                 ),
               ],
             ),
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => DetailsScreen(
-                snap: snap,
-              ),
-            )),
-            child: const Text(
-              "View",
-              style: TextStyle(
-                  color: primaryColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
