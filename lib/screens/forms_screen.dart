542ad792eb371d94f009e0a8518e03aa73e988e8
Sanchit <sanchitgurav100@gmail.com>
2023-03-17 09:35:21 +0530

    changes in form

--- lib/screens/forms_screen.dart
+++ lib/screens/forms_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../utils/colors.dart';
+import '../utils/utils.dart';
 
 class FormsPage extends StatefulWidget {
   @override
@@ -8,6 +14,14 @@ class FormsPage extends StatefulWidget {
 class _FormsPageState extends State<FormsPage> {
   final TextEditingController _textFieldController = TextEditingController();
   final TextEditingController _linkFieldController = TextEditingController();
+  Uint8List? _image;
+
+  void selectImage() async {
+    Uint8List img = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = img;
+    });
+  }
 
   @override
   void dispose() {
@@ -33,6 +47,73 @@ class _FormsPageState extends State<FormsPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 3, color: primaryColor),
+                      boxShadow: [
+                        BoxShadow(
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                          color: secondaryColor.withOpacity(0.1),
+                        ),
+                      ],
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://www.shutterstock.com/image-vector/blue-folder-icon-isolated-on-260nw-1501848521.jpg'),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      height: 45,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 3, color: Colors.white),
+                        color: primaryColor,
+                      ),
+                      child: IconButton(
+                        onPressed: selectImage,
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          color: blueColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Project Name:',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(
+                hintText: 'Enter project name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
             Text(
               'Description:',
               style: TextStyle(
@@ -40,6 +121,7 @@ class _FormsPageState extends State<FormsPage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(height: 5.0),
             TextField(
               controller: _textFieldController,
               decoration: InputDecoration(
@@ -47,7 +129,7 @@ class _FormsPageState extends State<FormsPage> {
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 20.0),
             Text(
               'Github Link:',
               style: TextStyle(
@@ -55,11 +137,14 @@ class _FormsPageState extends State<FormsPage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            TextField(
-              controller: _linkFieldController,
-              decoration: InputDecoration(
-                hintText: 'Enter a link',
-                border: OutlineInputBorder(),
+            SizedBox(height: 5.0),
+            Expanded(
+              child: TextField(
+                controller: _linkFieldController,
+                decoration: InputDecoration(
+                  hintText: 'Enter link',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
             SizedBox(height: 16.0),
@@ -69,7 +154,12 @@ class _FormsPageState extends State<FormsPage> {
                 onPressed: () {
                   // Do something with the form data
                 },
-                child: Text('Add'),
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
               ),
             ),
           ],
